c579499a339fe29441af330ac61043144d459604
Your Name <you@example.com>
2023-07-14 11:53:03 +0100

    prototypal inheritance and prototype chain

--- script.js
+++ script.js
@@ -6,16 +6,16 @@ const Person = function (firstName, birthYear) {
   this.birthYear = birthYear;
 
   // never do this
-  this.calcAge = function () {
-    console.log(2037 - this.birthYear);
-  };
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthYear);
+  //   };
 };
 
 const jonas = new Person('Jonas', 1991);
 console.log(jonas);
 
 // 1. New {} is created
-// 2. functional is called, this = ()
+// 2. function is called, this = ()
 // 3. {} linked to prototype
 // 4. function automatically return {}
 
@@ -24,3 +24,48 @@ const jack = new Person('Jack', 1975);
 console.log(matilda, jack);
 
 console.log(jonas instanceof Person);
+
+console.log(Person.prototype);
+
+Person.prototype.calcAge = function () {
+  console.log(2037 - this.birthYear);
+};
+jack.calcAge();
+jonas.calcAge();
+
+console.log(jonas.__proto__ === Person.prototype);
+
+// object can inherit properties from the constructor function
+Person.prototype.species = 'Homo sapiens';
+console.log(jonas.species);
+
+// we can check if a property is owned by the object or inherited from constructor function
+console.log(jonas.hasOwnProperty('firstName'));
+console.log(jonas.hasOwnProperty('species'));
+console.log(jonas);
+
+console.log(jonas.__proto__);
+// Object.prototype (top of prototype chain)
+console.log(jonas.__proto__.__proto__);
+console.log(jonas.__proto__.__proto__.__proto__);
+
+console.dir(Person.prototype.constructor);
+
+// Prototypal inheritance in built-in objects
+const arr = [3, 6, 6, 5, 6, 9, 9]; // new Array === []
+console.log(arr.__proto__);
+console.log(arr.__proto__ === Array.prototype);
+console.log(arr.__proto__.__proto__);
+console.log(arr.__proto__.__proto__.__proto__);
+
+Array.prototype.unique = function () {
+  return [...new Set(this)];
+};
+
+console.log(arr.unique());
+
+const h1 = document.querySelector('h1');
+console.dir(h1);
+
+// prototype in functions
+console.dir(x => x + 1);
